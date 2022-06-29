85376a3190cdaa3e65b9ee08ffe2e085185a76c3
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-06-29 19:41:53 -0300

    learnt user authentication, loggin in the user

--- tutorial_demo/src/pages/Login.js
+++ tutorial_demo/src/pages/Login.js
@@ -1,8 +1,26 @@
+import LoginForm from "../components/LoginForm";
+import {useHistory} from 'react-router-dom';
+
 function LoginPage(){
+
+    const history = useHistory();
+
+    // no need to specify the method handlers or anything. as fertch by default uses the "GET" method
+    function LoginUserHandler(user){
+        fetch('https://demoapi-12103-default-rtdb.firebaseio.com/users.json').then(() => (
+        // perform any task you want, such as verification of the password or anything which is desired   
+        history.replace('./home')));
+    }
+
+
+
     return(
-        <>
-        Login Page
-        </>
+        <section>
+            <h1>
+                Login Page
+            </h1>
+            <LoginForm loginUser = {LoginUserHandler}/>
+        </section>
     );
 }
 
