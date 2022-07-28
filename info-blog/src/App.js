f121223d5356b397ee932c0b5a7dc669195f38fd
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-07-28 14:14:55 -0300

    learnt about different components in React.js

--- info-blog/src/App.js
+++ info-blog/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React from 'react';
+import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
   
@@ -12,15 +14,9 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{ person }</p> */}
-        <p> { 10 }</p>
-        <p>{"Hello friends"}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{ Math.random()*10 }</p>
-        <a href={ link }>Google site</a>
+        <Home />
       </div>
     </div>
   );
