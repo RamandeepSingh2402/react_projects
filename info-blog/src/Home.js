4ba1a09e0317d22f72f02b5dd0375a78c38ef603
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-07-28 20:45:41 -0300

    learnt using the props to reuse the components

--- info-blog/src/Home.js
+++ info-blog/src/Home.js
@@ -11,8 +11,9 @@ const Home = () => {
     return ( 
         <div className="Home">
            <BlogList blogs={blogs} title="All blogs"/>
+           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
         </div>
      );
 }
- 
+
 export default Home;
