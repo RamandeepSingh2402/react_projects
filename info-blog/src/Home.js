d7ecf4a32238ff38438a32b5e02c461acb4fc7f6
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-07-30 19:18:31 -0300

    learnt about fetching the data using the get request

--- info-blog/src/Home.js
+++ info-blog/src/Home.js
@@ -2,27 +2,24 @@ import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My first blog', body: 'This is my first blog', author: 'mario', id: 1},
-        {title: 'My second blog', body: 'This is my second blog', author: 'yoshi', id: 2},
-        {title: 'My third blog', body: 'This is my third blog', author: 'mario', id: 3}
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('mario');
 
-    
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect(() =>{
-        console.log('use effect was run');
-    });
+        fetch('http://localhost:8000/blogs').then(res => {
+            return res.json();
+        })
+        .then((data) =>{
+            console.log(data);
+            setBlogs(data);
+        });
+    }, []);
 
     return ( 
         <div className="Home">
-           <BlogList blogs={blogs} title="All blogs" handleDelete = {handleDelete}/>
+           {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
      );
 }
